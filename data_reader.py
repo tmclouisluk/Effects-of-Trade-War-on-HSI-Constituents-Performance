d57ad18b82aabbb5ccd2a0fe060345bd3f74d724
tmclouisluk <failuk333@hotmail.com>
2018-12-21 17:16:37 +0000

    update map

--- data_reader.py
+++ data_reader.py
@@ -1,10 +1,12 @@
 import datetime
 import os
 import pandas as pd
+import numpy as np
 import json
-import slate3k as slate
-import PyPDF2
 import re
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import matplotlib.colors as co
 
 data_path = "./data"
 total_stock_path = './data/total/%s.pkl' % ("total")
@@ -48,3 +50,45 @@ countries = get_countries(countries_path)
 stock_list = read_stock_list(path_stock_list)
 total_stock_price = get_total_stock(total_stock_path)
 total_stock_analysis = get_total_stock_analysis(stock_list, pdf_analysis_path)
+
+country_stock_mean = total_stock_analysis.groupby(['Country', "lat", "lng", "Stock Code"])['Percent'].mean().reset_index()
+
+#
+# def plot_maps(frame, stock_list):
+#     for index, row in stock_list.iterrows():
+#         try:
+#             stock_code = row['Stock Code']
+#             m = Basemap(projection='cyl')
+#             scale = 0.2
+#             m.shadedrelief(scale=scale)
+#
+#             cmap = plt.cm.jet
+#             vmin = 0
+#             vmax = 100
+#
+#             data = frame.loc[frame['Stock Code'] == stock_code]
+#
+#             lon = [row['lng'] for index, row in data.iterrows()]
+#             lat = [row['lat'] for index, row in data.iterrows()]
+#             wind = [row['Percent'] for index, row in data.iterrows()]
+#             plt.scatter(lon, lat, marker='o',
+#                         c=wind, alpha=0.5, zorder=10, vmin=vmin, vmax=vmax, cmap=cmap)
+#
+#             try:
+#                 norm = co.Normalize(vmin=vmin, vmax=vmax)
+#                 pointcolors = plt.cm.ScalarMappable(norm, cmap)
+#                 pointcolors.set_array([])
+#                 cbar = m.colorbar(pointcolors, location='bottom')
+#                 cbar.set_label('%')
+#             except Exception as e:
+#                 pass
+#
+#             plt.title('Stock %s Country distribution' % stock_code)
+#             plt.show()
+#
+#
+#         except Exception as e:
+#             print(e)
+#
+#
+# plot_maps(country_stock_mean, stock_list)
